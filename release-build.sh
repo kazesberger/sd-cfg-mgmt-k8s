406efa247871b65154b472b31fa3265cf2401eb5
kazesberger <klaus.azesberger@bearingpoint.com>
2019-04-27 00:10:51 +0200

    gh-pages work now. release-script too :)

--- release-build.sh
+++ release-build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 ./install-reveal.js.sh
 
@@ -7,3 +7,14 @@ rm -rf target
 clojure -m figwheel.main -O simple -bo dev
 
 cp -r resources/public/* target/public/
+
+pushd target/public
+
+git init
+git add .
+git commit -m "update gh-pages"
+git push -f https://github.com/kazesberger/sd-cfg-mgmt-k8s.git master:gh-pages
+
+rm -rf .git
+
+popd
