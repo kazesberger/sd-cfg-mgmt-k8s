f074bf6abc60516567863a3adb8b69d1230c8888
kazesberger <klaus.azesberger@bearingpoint.com>
2019-04-26 09:42:58 +0200

    Wip Slide Deck: K8s Cfg Mgmt

--- src/reveal/slides.cljs
+++ src/reveal/slides.cljs
@@ -1,25 +1,152 @@
 (ns reveal.slides)
 
-(def slide-1
-  [:section
-   [:h1 "reveal-cljs"]
-   [:h3 "The ClojureScript-Wrapper for reveal.js"]
-   [:p "Based on "
-    [:a {:href "http://lab.hakim.se/reveal-js/"} "reveal.js"]]])
+(defn bulletpoints [items]
+  (let [li-attributes {:class "fragment"}]
+    (vec (concat [:ul] (map #(vector :li li-attributes %) items)))))
 
-(def slide-2
+(defn note [s]
+  [:aside {:class "notes"} s])
+
+(def title-slide
   [:section
-   [:section
-    [:h2 "Vertical Slides"]
-    [:p "Generate your slides "
-     [:a {:href "https://github.com/teropa/hiccups"} "with Hiccups"]]]
-   [:section
-    [:h2 "Tutorial"]
-    [:p "Watch the full tutorial of reveal.js "
-     [:a {:href "https://github.com/hakimel/reveal.js/blob/master/demo.html"} "on this site"]]]])
+   [:h1 "K8s Config Mgmt"]
+   [:h3 "Beginners guide"]])
+
+(def intro
+  [:section
+   [:h3 "this is how you DONT do presentations:"]])
+
+(def intro-2
+  [:section
+   [:p "This talk provides a beginner friendly introduction to the options available in the space of Kubernetes Configuration Management.\n  Kubernetes Configuration management is declarative and quite explicit. Also the Kubernetes API is extensive.\n  A valid Pod spec contains at least 30 distinct, valid child attributes (k8s v1.13).\n  There is a good chance specs will grow in their number and by numbers of fields.\n  This talk will give a short overview of the problem space and current options to manage configuration changes."]])
+
+(def intro-positive
+  [:section
+   [:h3 "things i like about k8s:"]
+   (bulletpoints
+     ["declarative"
+      "kubectl apply"
+      [:img {:src "img/make-it-so.jpg"}]])])
+
+(def imparative-vs-declarative
+  [:section
+   [:h3 "k8s object management"]
+   [:table {:style "white-space:nowrap;"}
+    [:thead {:style "font-weight: bold;"}
+     [:th "technique"]
+     [:th "operates on"]
+     [:th "learning curve"]]
+    [:tbody
+     [:tr [:td "imparative commands"][:td "live objects"][:td "lowest"]]
+     [:tr [:td "imperative obj config"][:td "individual files"][:td "moderate"]]
+     [:tr [:td "declarative obj config"][:td "Directories of files"][:td "highest"]]]]])
+
+(def imparative-vs-declarative-2
+  [:section
+   [:h3 "k8s object management"]
+   [:table {:style "white-space:nowrap; width:auto;"}
+    [:thead {:style "font-weight: bold;"}
+     [:th "technique"]
+     [:th "example"]]
+    [:tbody
+     [:tr [:td "imp cmd"] [:td "kubectl run nginx --image nginx"]]
+     [:tr [:td "imp obj config"] [:td "kubectl create -f nginx.yaml"]]
+     [:tr [:td ""] [:td "kubectl delete -f nginx.yaml"]]
+     [:tr [:td ""] [:td "kubectl replace -f nginx.yaml"]]
+     [:tr [:td "decl obj config"][:td "kubectl apply -R -f configs/"]]]]])
+
+(def imparative-vs-declarative-3
+  [:section
+   [:h3 "recap: declarative cfg"]
+   (bulletpoints
+     ["forget about the HOW, the parts and infra"
+      "manipulate state by declaration of desired state"
+      "utilize git for these declarations"
+      "PR-Workflow gives you 'GitOps'"
+      "we like this - let's not lose it on our journey"])])
+
+(def sounds-ez
+  [:section {:class "fragment"}
+   [:h4 "sounds doable!"]
+   [:ol
+    [:li {:class "fragment"} "Write the App"]
+    [:li {:class "fragment"} "Describe its parts using 'Kubernetes-Objects'"]
+    [:li {:class "fragment"} "kubectl apply -f config/"]]
+   [:img {:src "img/loc-msvc-demo.png" :class "fragment"}]])
+
+(def fun-begins ; TODO make these pictures not bulletpoints
+  [:section
+   [:h4 "what if..."]
+   (bulletpoints
+     [[:img {:src "img/stages.png"}]
+      "Customizations"
+      "..."])])
+
+(def copy-pasta
+  [:section
+   [:img {:src "img/inventory-1.png"}]])
+
+(def templates
+  [:section
+   [:pre
+     "apiVersion: v1\nkind: Service\nmetadata:\n  name: {{ template \"minecraft.fullname\" . }}\n  labels:\n    app: {{ template \"minecraft.fullname\" . }}\n    chart: \"{{ .Chart.Name }}-{{ .Chart.Version }}\"\n    release: \"{{ .Release.Name }}\"\n    heritage: \"{{ .Release.Service }}\"\nspec:\n  type: {{ .Values.minecraftServer.serviceType }}\n  ports:\n  - name: minecraft\n    port: 25565\n    targetPort: minecraft\n    protocol: TCP\n  selector:\n    app: {{ template \"minecraft.fullname\" . }}"]
+    ;[:code {:class "hljs" :data-trim "" :data-noescape ""} "apiVersion: v1\nkind: Service\nmetadata:\n  name: {{ template \"minecraft.fullname\" . }}\n  labels:\n    app: {{ template \"minecraft.fullname\" . }}\n    chart: \"{{ .Chart.Name }}-{{ .Chart.Version }}\"\n    release: \"{{ .Release.Name }}\"\n    heritage: \"{{ .Release.Service }}\"\nspec:\n  type: {{ .Values.minecraftServer.serviceType }}\n  ports:\n  - name: minecraft\n    port: 25565\n    targetPort: minecraft\n    protocol: TCP\n  selector:\n    app: {{ template \"minecraft.fullname\" . }}"]]
+   [:img {:src "img/helm-template"}]])
+
+(def template-downsides
+  [:section
+   [:h4 "downsides of templating"]
+   (bulletpoints
+     ["we need to change the manifests"
+      "we need to provide the values"])])
+
+; TODO short helm explanation and show templates
+; TODO show folder full of values.yaml files (lack of proper inventory)
+
+(def template-downsides-no-big-deal-aight
+  [:section
+   [:h4 "change the manifests?"
+    (bulletpoints
+      ["bespoken apps"
+       "Common Off-The-Shelf (COTS) apps"])]])
+
+
+(def kustomize
+  [:section
+   [:h4 "kustomize to the rescue"]
+   (bulletpoints
+     ["templating without placeholders"
+      "this ain't templating!!!11"
+      "...that's a good thing :)"
+      "but we're still having "])])
+
+(def controller-first-encounter
+  [:section
+   [:p "we declare stuff, then..."]
+   [:img {:src "img/magical-dr-evil.jpg"}]
+   [:p "the state is as we desired?"]])
+
+(def controllers-alien-meme
+  [:section
+   [:img {:src "img/controllers-alien.jpg"}]])
+
+(def cots-and-bespoken-apps)
+
 
 (defn all
   "Add here all slides you want to see in your presentation."
   []
-  [slide-1
-   slide-2])
+  [title-slide
+   intro
+   intro-2
+   intro-positive
+   imparative-vs-declarative
+   imparative-vs-declarative-2
+   imparative-vs-declarative-3
+   sounds-ez
+   fun-begins
+   copy-pasta
+   templates])
+
+   ;controller-first-encounter
+   ;controllers-alien-meme])
---
+++ todo.md
@@ -0,0 +1,74 @@
+# plan
+
+## notes
+
+* tweet public slide deck (calm the niggrs #pulpfiction by mentioning/showing this on one of first and last slides)
+* ask for feedback (upfront) -> blog post
+* 
+
+## topics
+
+* [local kubeconfig](https://medium.com/@ahmetb/mastering-kubeconfig-4e447aa32c75)
+* useful links
+* category distinction: bespoken vs COTS (common off the shelf) apps
+* list of k8s app mgmt tools
+* helm? -> not that different to kapitan but less complicated
+* xxx as Code?
+* template yaml?
+* patching manifests?
+* patching helm charts? https://testingclouds.wordpress.com/2018/07/20/844/
+* kapitan
+* kr8
+* ship
+* state vs statless apps -> operators
+
+## ablauf
+
+//* not battle-tested knowledge -> more like ivory tower -> more like hyde park speakers corner speaker
+* k8s introduction like talk summary
+* we don't like problems so let's start positive: what do i appreciate about k8s up to now:
+* kubectl commands imparative vs declarative -> kubectl apply -> yay (make it so!)
+    * https://kubernetes.io/docs/concepts/overview/object-management-kubectl/overview/
+* forget about infra -> manipulate state by declaration of desired state -> git -> operate by PR -> gitops
+* we like this declarative thing - let's not lose it on our way
+* well this sounds easy, until -> Wall of Yaml-files crits over 9k
+    * maybe give a short overview of loc for manifest files of some COTS services
+    * but this isn't where it ends, we might have multiple (slightly different) instances of the same thing
+
+    * so we need something like an inventory, config that makes our app instances special.
+    * we want to avoid copy/pasting files (while slightly modifiying them)
+    * we want to preserve the ability to easily upgrade the core parts (eg. rebase on new upstream version)
+* downsides of templating (like in helm/values.yaml or by jsonnet-based tools)
+    * we need to build the template (bespoken vs COTS)
+    * we need to provide the values
+        * dealing with optional values
+        * dealing with conditions
+* perks of patching with kustomize?
+    * kustomize works with the original manifest files - we can work with unmodified upstream and easily upgrade anytime
+* combined powers #cptnPlanet -> kapitanOverlayed
+    * inventory: reclass
+    * we only generate values.yaml and/or kustomize patches
+    * thus we can easily rebase new upstream versions anytime
+
+* so now we've got a workflow
+* does it still work for stateful service operations world?
+    * name examples of popular/common services
+    * so we have situations where we need a special procedure to establish desired state
+    * and we might even have a somewhat dynamic desired state.
+* operators:
+    * a design pattern built upon the controller pattern
+    ```
+    In applications of robotics and automation, a control loop is a non-terminating loop 
+    that regulates the state of the system. In Kubernetes, a controller is a control loop 
+    that watches the shared state of the cluster through the API server and makes changes 
+    attempting to move the current state towards the desired state. Examples of controllers 
+    that ship with Kubernetes today are the replication controller, endpoints controller, 
+    namespace controller, and serviceaccounts controller.
+    ```
+    * an operator 
+        * watches
+        * analyzes
+        * acts
+    * and the operator does this
+        * with single-app focus
+        * utilizing CRDs
